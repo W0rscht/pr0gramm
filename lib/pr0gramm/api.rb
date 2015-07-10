26f16383ade5fba2b9b152cbc7f41ee75048f341
W0rscht <W0rscht@users.noreply.github.com>
2015-07-10 22:31:38 +0200

    Added method to fetch user inbox.

--- lib/pr0gramm/api.rb
+++ lib/pr0gramm/api.rb
@@ -23,6 +23,34 @@ class Pr0gramm
       Pr0gramm::User.new(self, result)
     end
 
+    def inbox( inbox_type = :all )
+
+      fail 'Not logged in.' if !session
+
+      result = @requester.api_get("/inbox/#{inbox_type}")
+
+      # TODO: result['hasNewer']
+      # TODO: result['hasOlder']
+
+      @messages = []
+      result['messages'].each { |message|
+
+        next if message['itemId']
+        # TODO: comment lookup to Pr0gramm::Comment
+        # @item_id    = message['itemId']
+        # @name       = message['name']
+        # @mark       = Pr0gramm::Mark.string( message['mark'] )
+        # @score      = message['score']
+        # @thumb_path = message['thumb']
+        # @thumb_url  = "#{@api.requester.thumbs_url}#{thumb_path}"
+
+        @messages.push( Pr0gramm::Message.new( self, message ) )
+      }
+
+      @messages
+    end
+
+
     def items( parameter = {} )
 
       parameter = {
