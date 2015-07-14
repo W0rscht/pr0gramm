670e7a92b8d0be0c940e80ede52ec48ffba2ec8a
W0rscht <W0rscht@users.noreply.github.com>
2015-07-14 17:26:40 +0200

    Added initial version of upload method.

--- lib/pr0gramm/api/item.rb
+++ lib/pr0gramm/api/item.rb
@@ -1,6 +1,16 @@
+require 'uri'
+require 'mime/types'
+
 class Pr0gramm
   module API
     module Item
+
+      ALLOWED_UPLOAD_TYPES = [
+          'image/png',
+          'image/jpeg',
+          'image/gif',
+          'video/webm'
+      ]
 
       def items( parameter = {} )
 
@@ -48,6 +58,71 @@ class Pr0gramm
           comments: comments,
           tags:     tags
         }
+      end
+
+      def upload(file, flag, tags, check_similar = true)
+
+        session_data = @requester.session
+
+        fail 'Not logged in.' if !session_data
+
+        flag_state = flag.to_s
+        if flag_state != 'sfw'
+          tags.push(flag_state)
+        end
+
+        parameter = {
+          _nonce:       session_data[:nonce],
+          tags:         tags.join(','),
+          sfwstatus:    flag_state,
+          checkSimilar: check_similar ? 1 : 0,
+        }
+
+        # URL
+        if file =~ /\A#{URI::regexp(['http', 'https'])}\z/
+          parameter[:imageUrl] = file
+        else
+          mime = MIME::Types.type_for file
+
+          mime_type = 'unknown'
+          if !mime.empty? && mime[0].content_type
+            mime_type = mime[0].content_type
+          end
+
+          if !ALLOWED_UPLOAD_TYPES.include?( mime_type )
+            fail "Invalid mime type '#{mime_type}' for file '#{file}'."
+          end
+
+          file_size_mb = File.size( file ).to_f / 2**20
+
+          if file_size_mb > 4.0
+
+
+            if !session_data[:paid] || file_size_mb > 8.0
+              fail "File '#{file}' is to big: #{file_size_mb} MB."
+            end
+          end
+
+          # TODO: resoultion, megapixel, duration
+
+          result = @requester.api_post('/items/upload', { image: File.new(file, 'rb') })
+
+          parameter[:key] = result['key']
+        end
+
+        result = @requester.api_post('/items/post', parameter)
+
+        # TODO: Pr0gramm::Item.new?
+        if result['similar']
+          return result['similar']
+        end
+
+        if result['error']
+          fail "Upload error: #{result['error']}."
+        end
+
+        # "{\"error\"=>nil, \"selfPosted\"=>true, \"item\"=>{\"id\"=>893147}, \"ts\"=>1436885732, \"cache\"=>nil, \"rt\"=>733, \"qc\"=>53}"
+        return result['item']['id']
       end
     end
   end
