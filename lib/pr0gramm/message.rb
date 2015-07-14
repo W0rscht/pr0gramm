69fb4dc1be3f2ff9058a5472b797018deabeff54
W0rscht <W0rscht@users.noreply.github.com>
2015-07-14 19:06:51 +0200

    Reworked to current API version.

--- lib/pr0gramm/message.rb
+++ lib/pr0gramm/message.rb
@@ -1,8 +1,7 @@
 class Pr0gramm
   class Message
 
-    attr_reader :id, :message, :sender_id, :created, :sent, :sender_name, :sender_mark,
-                :recipient_name, :recipient_mark, :recipient_id
+    attr_reader :id, :message, :sender_id, :sender_name, :sender_mark, :created
 
     def initialize(api, message_data)
 
@@ -11,13 +10,9 @@ class Pr0gramm
       @id             = message_data['id']
       @message        = message_data['message']
       @sender_id      = message_data['senderId']
+      @sender_name    = message_data['name']
+      @sender_mark    = Pr0gramm::Mark.string( message_data['mark'] )
       @created        = Time.at( message_data['created'].to_i ).to_datetime
-      @sent           = message_data['sent']
-      @sender_name    = message_data['senderName']
-      @sender_mark    = Pr0gramm::Mark.string(  message_data['senderMark'] )
-      @recipient_name = message_data['recipientName']
-      @recipient_mark = Pr0gramm::Mark.string(  message_data['recipientMark'] )
-      @recipient_id   = message_data['recipientId']
     end
   end
 end
